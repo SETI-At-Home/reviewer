b727c0a7939a39555944a6395c9b8e9946b96b11
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2017-11-06 11:45:44 +0100

    fixed redirect url

--- server/services/passport.js
+++ server/services/passport.js
@@ -16,26 +16,29 @@ passport.deserializeUser((id, done) => {
         });
 });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({
-            googleId: profile.id
-        })
-        .then((existingUser) => {
-            if (existingUser) {
-                // we already have a record with the given profile id
-                done(null, existingUser);
-            } else {
-                // we dont have user record with this user id and make record
-                new User({
-                        googleId: profile.id
-                    })
-                    .save()
-                    .then(user => done(null, user));
-            }
-        })
+passport.use(
+    new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({
+                    googleId: profile.id
+                })
+                .then((existingUser) => {
+                    if (existingUser) {
+                        // we already have a record with the given profile id
+                        done(null, existingUser);
+                    } else {
+                        // we dont have user record with this user id and make record
+                        new User({
+                                googleId: profile.id
+                            })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                })
 
-}));
+        }));
